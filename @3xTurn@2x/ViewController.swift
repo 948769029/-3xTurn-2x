7c5cded49aa34b0c9b09cb01c8c2d211bfd2bf53
948769029 <apple@appledeMacBook-Pro.local>
2017-11-08 14:25:55 +0800

    Initial Commit

---
+++ @3xTurn@2x/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  @3xTurn@2x
+//
+//  Created by apple on 2017/11/8.
+//  Copyright © 2017年 张洪健. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ @3xTurn@2xTests/_3xTurn_2xTests.swift
@@ -0,0 +1,36 @@
+//
+//  _3xTurn_2xTests.swift
+//  @3xTurn@2xTests
+//
+//  Created by apple on 2017/11/8.
+//  Copyright © 2017年 张洪健. All rights reserved.
+//
+
+import XCTest
+@testable import _3xTurn_2x
+
+class _3xTurn_2xTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
